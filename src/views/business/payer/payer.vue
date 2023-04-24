edfa38cda2df881d89caa42326156bb1b05f1016
xiaoqinwa <1448715904@qq.com>
2023-04-24 11:13:12 +0800

    fix: 付款账户管理

---
+++ src/views/business/payer/payer.vue
@@ -0,0 +1,127 @@
+<!--付款人管理-->
+<template>
+  <d2-container>
+    <template slot="header">
+      <div style="float:left;padding-top:3px">
+        <el-input @keyup.enter.native="handleFilter" clearable style="width: 200px;margin-right: 20px;" class="filter-item" placeholder="付款人搜索" v-model="filterFormData.payer" size="small"> </el-input>
+        <el-button class="filter-item" type="primary" icon="el-icon-search" @click="handleRefresh" size="small">搜索</el-button>
+        <el-button class="filter-item" type="primary" icon="el-icon-refresh" @click="handleRefresh" size="small">刷新</el-button>
+      </div>
+      <div style="float:right;">
+        <el-button icon="vxe-icon-square-plus" type="success" size="medium" style="width:100px" @click="handleCreate">新增</el-button>
+        <el-button type="primary" size="medium" style="width:100px" @click="pageList()">刷新</el-button>
+      </div>
+    </template>
+
+    <template>
+      <vxe-toolbar ref="vxeToolBarRef" style="height:8%" :refresh="{query: pageList}" custom zoom>
+        <template #buttons>
+          <el-button icon="vxe-icon-table" size="mini" style="width:120px">付款人管理</el-button>
+          <!-- <el-button icon="vxe-icon-chart-pie" size="mini" style="width:120px" @click="filterDialogVisibleClick">过滤数据</el-button> -->
+        </template>
+      </vxe-toolbar>
+      <vxe-table
+        class="mytable-scrollbar"
+        size="medium"
+        header-cell-class-name="headerClassName"
+        cell-class-name="cellClassName"
+        border
+        resizable
+        show-overflow
+        show-header-overflow
+        v-loading.body="listLoading"
+        ref="vxeTableRef"
+        height="92%"
+        :row-config="{isHover: true}"
+        :data="tableData">
+        <vxe-column type="checkbox" width="45"></vxe-column>
+        <vxe-column type="seq" title="序号" width="60"></vxe-column>
+        <vxe-column field="payer" title="付款人名称" width="200"></vxe-column>
+        <vxe-column field="payAccount" title="付款账户" width="200"></vxe-column>
+        <vxe-column title="操作" width="160" fixed="right" show-overflow>
+          <template #default="{ row }">
+            <vxe-button type="text" status="success" icon="vxe-icon-edit" @click="handleUpdate(row)">修改</vxe-button>
+            <vxe-button type="text" status="danger" icon="vxe-icon-delete" @click="handleRemove(row)">删除</vxe-button>
+          </template>
+        </vxe-column>
+      </vxe-table>
+      <div v-show="dialogFormVisible" width="40%">
+        <vxe-modal v-if="dialogFormVisible" title="新增数据页面" v-model="dialogFormVisible" :visible.sync="dialogFormVisible"
+        @close="createModalClose" width="40%">
+          <vxe-form ref="createFrom" title-width="120" title-align="right" titleColon
+          :data="createFormData" :items="createForm" :rules="createFromRules"
+          @submit="handleSubmitCreate('createFrom')" @reset="handleCancelCreate('createFrom')">
+          </vxe-form>
+        </vxe-modal>
+      </div>
+    </template>
+
+    <template slot="footer">
+      <el-pagination @size-change="handleSizeChange" @current-change="handleCurrentChange" :current-page.sync="filterFormData.currentPage" :page-sizes="[10,20,30, 50]" :page-size="filterFormData.pageSize" layout="total, sizes, prev, pager, next, jumper" :total="total"> </el-pagination>
+    </template>
+  </d2-container>
+</template>
+
+<script>
+import mixins from '@/mixin/commonMixin.js'
+import { myMethods } from './js/payerMethod.js'
+export default {
+  name: 'payer',
+  mixins: [mixins],
+  data() {
+    return {
+      dialogStatus: '',
+      dialogFormVisible: false,
+      tableData: [],
+      listLoading: false,
+      total: null,
+      filterFormData: {
+        currentPage: 1,
+        pageSize: 20,
+        payer: ''
+      },
+      createFormData: {
+        payer: '',
+        payAccount: ''
+      },
+      createFromRules: {
+        payer: [
+          { required: true, message: '请输入付款人', trigger: 'blur' }
+        ],
+        payAccount: [
+          { required: true, message: '请输入付款账户', trigger: 'blur' }
+        ]
+      },
+      createForm: [
+        {
+          title: '',
+          span: 23,
+          children: [
+            { field: 'payer', title: '付款人名称', span: 24, itemRender: { name: '$input', props: { placeholder: '请输入付款人名称' } } },
+            { field: 'payAccount', title: '付款账户', span: 24, itemRender: { name: '$input', props: { placeholder: '请输入付款账户' } } }
+          ]
+        },
+        {
+          align: 'right',
+          span: 23,
+          itemRender: {
+            name: '$buttons',
+            children:
+              [{ props: { type: 'submit', content: '保存', status: 'primary' } },
+                { props: { type: 'reset', content: '取消', status: 'warning' } }]
+          }
+        }
+      ]
+    }
+  },
+  methods: {
+    ...myMethods
+  },
+  created() {
+    this.initCreated()
+  },
+  mounted() {
+    this.initMounted()
+  }
+}
+</script>
